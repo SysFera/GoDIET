65b3ed1fab8dfe5017c6c62a01f4b6e3089a287d
hdail <hdail>
2004-05-25 08:44:51 +0000

    Added interactive GoDIET shell capability with support for launch, history, help, stop, status and exit.  Non-interactive launch is supported with --launch command line switch.

--- goDiet/GoDIET.java
+++ goDiet/GoDIET.java
@@ -8,6 +8,8 @@ package goDiet;
 
 import goDiet.Controller.*;
 
+import java.io.*;
+
 /**
  *
  * @author  hdail
@@ -18,27 +20,91 @@ public class GoDIET {
     }
     
     protected static final String USAGE = 
-        "USAGE: GoDiet <file.xml>\n";  
+        "USAGE: GoDiet [--launch] <file.xml>";
+    protected static final String HELP = 
+        "The following commands are available:\n" +
+        "   launch:     launch entire DIET platform\n" +
+        "   stop:       kill entire DIET platform using kill pid\n" +
+//        "   kill:       kill entire DIET platform using kill -9 pid\n" +
+        "   status:     print run status of each DIET component\n" +
+        "   history:    print history of commands executed\n" +
+        "   help:       print this message\n" +
+        "   exit:       exit GoDIET, do not change running platform.";    
     
-    public static void main(String[] args) {       
-        System.out.println("\n*** Running GoDIET.");
-
-        if( args.length != 1 ) {
+    public static void main(String[] args) {
+        boolean intShell = true;
+        String xmlFile = "";
+        int i;
+        
+        if( (args.length < 1) || (args.length > 2) ) {
             System.err.println(USAGE);
             System.exit(1);
         }
         
+        /** Parse Arguments */
+        xmlFile = args[args.length - 1];    // last always XML
+        for(i = 0; i < args.length; i++){
+            if(args[i].compareTo("--launch") == 0){
+                intShell = false;
+            } 
+        }
+        
+        System.out.println("*** GoDIET.");
+   
+        /** Prepare GoDIET for usage */
         DietPlatformController mainController = 
                 new DietPlatformController();
 
-        System.out.println("\n* Parsing input XML file.");
-        String xmlFile = args[0];
+        System.out.println("\n* Trying to parse input XML file " + xmlFile);
         mainController.parseXmlFile(xmlFile);
         
-        System.out.println("\n* Launching DIET platform.");
-        mainController.launchPlatform();
-        
-        System.out.println("GoDIET finished.");
-    }
-    
+        if(!intShell){
+            // If not interactive, just launch platform and exit
+            System.out.println("* Launching DIET platform.");
+            mainController.launchPlatform();
+            System.out.println("GoDIET finished.");
+        } else {
+            BufferedReader stdin = new BufferedReader(
+                new InputStreamReader(System.in));
+            String command;
+            java.util.Vector history = new java.util.Vector();
+            while(true){
+                System.out.print("GoDIET> ");
+                command = "";
+                try {
+                    command = stdin.readLine();
+                } catch(Exception x) {
+                    break;
+                }
+                if(command == null)
+                    break;
+                command = command.trim();
+                if(command.equals(""))
+                    continue;
+                history.addElement(command);
+                if(command.equals("history")){
+                    for(i = 0; i < history.size(); i++){
+                        System.out.println(i + " " + 
+                            (String)history.elementAt(i));
+                    }
+                } else if(command.equals("launch")){
+                    System.out.println("* Launching DIET platform.");
+                    mainController.launchPlatform();  
+                } else if(command.equals("stop")){
+                    System.out.println("* Stopping DIET platform.");
+                    mainController.stopPlatform();
+               /* } else if(command.equals("kill")){
+                    System.out.println("* Killing DIET platform.");
+                    // TODO*/
+                } else if(command.equals("status")){
+                    mainController.printPlatformStatus();
+                } else if(command.equals("exit")){
+                    System.out.println("* Exiting GoDIET. Bye.");
+                    break;
+                } else {
+                    System.out.println(HELP);
+                }
+            }
+        }
+    }  // End of GoDIET main(). 
 }
