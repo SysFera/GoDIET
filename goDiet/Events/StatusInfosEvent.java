4b65ccfad38aa4b59e15aee81b970fbaffa2dfce
rbolze <rbolze>
2004-06-16 12:16:40 +0000

    when some one ask for Status info

---
+++ goDiet/Events/StatusInfosEvent.java
@@ -0,0 +1,20 @@
+/*
+ * StatusInfosEvent.java
+ *
+ * Created on 15 juin 2004, 23:43
+ */
+
+package goDiet.Events;
+
+/**
+ *
+ * @author  rbolze
+ */
+public class StatusInfosEvent extends java.awt.AWTEvent{
+    
+    /** Creates a new instance of StatusInfosEvent */
+    public StatusInfosEvent(java.lang.Object source) {
+        super(source, 110);
+    }
+    
+}
