78b95e38d4acf40d1c54c91b9b4f0394f6680216
hdail <hdail>
2004-07-13 14:12:19 +0000

    Event for communicating current launch request.

---
+++ goDiet/Events/LaunchRequest.java
@@ -0,0 +1,25 @@
+/*
+ * LaunchRequest.java
+ *
+ * Created on 26 June 2004, 15:01
+ */
+
+package goDiet.Events;
+
+/**
+ *
+ * @author  rbolze
+ */
+public class LaunchRequest extends java.awt.AWTEvent {
+    private String launchRequest;
+    
+    /** Creates a new instance of AddElementsEvent */
+    public LaunchRequest(java.lang.Object source, String launchRequest) {
+        super(source, 110);
+        this.launchRequest = launchRequest;
+    }
+    
+    public String getLaunchRequest(){
+        return this.launchRequest;
+    }   
+}
