38a637881011f9e4998432ab5a0c1df27499e408
hdail <hdail>
2004-07-13 14:09:24 +0000

    Fairly major re-factor / improvements: - Moved launch-related control to DeploymentController, which now has   own thread, allowing real-time text updates in graphical mode - Added communication with LogCentral for feedback about the   run-time status of components.  LogCentralCommController controls   this process, and has its own thread - Launch timing is based on communications from log central, when   available - Cleaned up the model, esp for run-time attributes

---
+++ goDiet/Utils/CORBA/ToolMsgReceiverImpl.java
@@ -0,0 +1,24 @@
+package goDiet.Utils.CORBA;
+
+public class ToolMsgReceiverImpl extends ToolMsgReceiverPOA {
+    goDiet.Utils.LogCentralConnection logCentralConnection;
+    public ToolMsgReceiverImpl(goDiet.Utils.LogCentralConnection logConnect) {
+        this.logCentralConnection = logConnect;
+        this._initialize_inheritance_tree();
+    }
+    
+    public void sendMsg(log_msg_t[] msgBuf) {
+        for (int i = 0 ; i < msgBuf.length ; i++) {
+            //sendMsg(msgBuf[i]);
+            //System.out.println("SendMsg");
+            logCentralConnection.recieveMsg(msgBuf[i]);
+        }
+        throw new UnsupportedOperationException();
+    }
+    
+    public void _initialize_inheritance_tree() {//GEN-BEGIN:M_void__initialize_inheritance_tree_
+        // Do not edit! This is a method which is necessary for using delegation.
+    }//GEN-END:M_void__initialize_inheritance_tree_
+    
+    
+}
