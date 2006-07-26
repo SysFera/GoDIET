e092e69048b29f0c38a68a479ee42e008de97cdb
rbolze <rbolze>
2006-07-26 15:04:55 +0000

    this object define the Ma_dag element

---
+++ goDiet/Model/Ma_dag.java
@@ -0,0 +1,26 @@
+/*
+ * MasterAgent.java
+ *
+ * Created on 13 avril 2004, 15:24
+ */
+
+package goDiet.Model;
+
+/**
+ *
+ * @author  rbolze
+ */
+public class Ma_dag extends Agents {    
+    private Agents parent;
+    
+    /** Creates a new instance of MasterAgent */
+    public Ma_dag(String name, ComputeResource compRes, String binary
+            ,Agents parent){
+        super(name, compRes, binary);
+        this.parent=parent;
+    }
+    
+    public Agents getParent(){
+        return this.parent;
+    }
+}
