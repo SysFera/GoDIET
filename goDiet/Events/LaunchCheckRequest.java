46c5127b7efdcc103aea4335a8a3ac561358ec45
dloureir <dloureir>
2009-08-11 11:54:05 +0000

    Adding launch_check command : event needed for the launch check command implementaiton inside the DeployController

---
+++ goDiet/Events/LaunchCheckRequest.java
@@ -0,0 +1,26 @@
+/*@GODIET_LICENSE*/
+/*
+ * LaunchRequest.java
+ *
+ * Created on 26 June 2004, 15:01
+ */
+
+package goDiet.Events;
+
+/**
+ *
+ * @author  rbolze
+ */
+public class LaunchCheckRequest extends java.awt.AWTEvent {
+    private String launchAndRequest;
+    
+    /** Creates a new instance of AddElementsEvent */
+    public LaunchCheckRequest(java.lang.Object source, String launchAndRequest) {
+        super(source, 110);
+        this.launchAndRequest = launchAndRequest;
+    }
+    
+    public String getLaunchAndRequest(){
+        return this.launchAndRequest;
+    }   
+}
