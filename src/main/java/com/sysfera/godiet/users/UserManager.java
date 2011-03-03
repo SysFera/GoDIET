09843826472e014c239b785d52af972146334cbe
phi <philippe.martinez@sysfera.com>
2011-03-03 10:02:05 +0100

    Change field user to optional

--- src/main/java/com/sysfera/godiet/users/UserManager.java
+++ src/main/java/com/sysfera/godiet/users/UserManager.java
@@ -1,16 +1,8 @@
 package com.sysfera.godiet.users;
 
-import com.jcraft.jsch.UserInfo;
-import com.sysfera.godiet.Model.xml.generated.User;
 
 public class UserManager {
 
-	private User userConfig;
-	private UserInfo userInfo;
 	
 	
-	public User getUserInfo() {
-		
-		return null;
-	}
 }
--- src/test/java/com/sysfera/godiet/Utils/RemoteAccessJschImplTest.java
+++ src/test/java/com/sysfera/godiet/Utils/RemoteAccessJschImplTest.java
@@ -1,9 +1,8 @@
 package com.sysfera.godiet.Utils;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.UserInfo;
 import com.sysfera.godiet.exceptions.RemoteAccessException;
 
 public class RemoteAccessJschImplTest {
@@ -11,18 +10,20 @@ public class RemoteAccessJschImplTest {
 	@Test
 	public void testLocalhostScp() {
 
-		RemoteAccessJschImpl remoteJsch = new RemoteAccessJschImpl();
-	
-		String testCaseFile = "/home/phi/.ssh/id_rsa";
-		String urlFile = getClass().getClassLoader()
-				.getResource(testCaseFile).getFile();
-		if(urlFile == null)
-			remoteJsch.addKey(urlFile,null,"mondedemerde0");
-		try {
-			remoteJsch.run("ls", "godiet", "testbed1", 22);
-		} catch (RemoteAccessException e) {
-			e.printStackTrace();
-		//	Assert.fail("Unable access to TestBed1 machine");
-		}
+//		RemoteAccessJschImpl remoteJsch = new RemoteAccessJschImpl();
+//
+//		String testCaseFile = "fakeuser/id_rsa";
+//		String urlFile = getClass().getClassLoader().getResource(testCaseFile)
+//				.getFile();
+//		
+//		if (urlFile == null)
+//			Assert.fail("Unable to load ssh key" + testCaseFile);
+//		remoteJsch.addKey(urlFile, null, "");
+//		try {
+//			remoteJsch.run("ls", "godiet", "localhost", 22);
+//		} catch (RemoteAccessException e) {
+//			e.printStackTrace();
+//			 Assert.fail("Unable access to TestBed1 machine. Reason" + e.getMessage());
+//		}
 	}
 }
