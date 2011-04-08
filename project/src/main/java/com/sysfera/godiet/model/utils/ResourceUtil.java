3afeb161de5044b084e1bbb01eb642adb1e13db9
pmartinez <philippe.martinez@sysfera.com>
2011-04-08 20:08:24 +0200

    Add helper to get resource ENV

---
+++ project/src/main/java/com/sysfera/godiet/model/utils/ResourceUtil.java
@@ -0,0 +1,25 @@
+package com.sysfera.godiet.model.utils;
+
+import java.util.List;
+
+import com.sysfera.godiet.model.generated.Resource;
+import com.sysfera.godiet.model.generated.Var;
+
+public class ResourceUtil {
+
+	/**
+	 * Search the value in Node's environment variables
+	 * @param node
+	 * @param varName
+	 * @return the value or null if it varName doesn't exist
+	 */
+	public static String getEnvValue(final Resource node,final String varName)
+	{
+		List<Var> vars = node.getEnv().getVar();
+		for (Var var : vars) {
+			if(var.getName().equals(varName))
+				return var.getValue();
+		}
+		return null;
+	}
+}
