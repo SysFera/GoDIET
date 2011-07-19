94d56d925258b8a27245fe6965dfa332ab60d86b
pmartinez <philippe.martinez@sysfera.com>
2011-07-19 16:58:28 +0200

    Change source node parameter (mylaptop->graal)

--- integration-test/src/test/java/com/sysfera/godiet/command/LaunchPlatformIntegrationTest.java
+++ integration-test/src/test/java/com/sysfera/godiet/command/LaunchPlatformIntegrationTest.java
@@ -63,7 +63,7 @@ public class LaunchPlatformIntegrationTest {
 		try {
 			{
 				// Loading configuration
-				String configurationFile = "configuration/configuration-local.xml";
+				String configurationFile = "configuration/configuration.xml";
 
 				InputStream inputStream = getClass().getClassLoader()
 						.getResourceAsStream(configurationFile);
