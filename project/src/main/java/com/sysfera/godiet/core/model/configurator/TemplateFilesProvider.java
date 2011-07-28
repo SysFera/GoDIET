0163fcba935d2ed00e5833850bc0516516213697
pmartinez <philippe.martinez@sysfera.com>
2011-07-28 11:03:35 +0200

    Exposition of configuration templates files to the user. Server distribution now create templtes directory which include all templates. Could update (at runtime) template files

--- project/src/main/java/com/sysfera/godiet/core/model/configurator/TemplateFilesProvider.java
+++ project/src/main/java/com/sysfera/godiet/core/model/configurator/TemplateFilesProvider.java
@@ -13,37 +13,45 @@ import freemarker.cache.ClassTemplateLoader;
 import freemarker.cache.FileTemplateLoader;
 import freemarker.cache.MultiTemplateLoader;
 import freemarker.cache.TemplateLoader;
+
 /**
  * Manage and provide the configuration files found in {HOME}/.godiet/templates
- * and {JAR}/configuration/templates directory.
- * Use Freemarker implementation
+ * and {JAR}/configuration/templates directory. Use Freemarker implementation
+ * 
  * @author phi
- *
+ * 
  */
 @Service
 public class TemplateFilesProvider {
 	private Logger log = LoggerFactory.getLogger(getClass());
-
+	private static String TEMPLATE_DIRECTORY_PATH = System
+			.getenv("TEMPLATE_DIRECTORY_PATH");
 	private final MultiTemplateLoader multiTemplateLoader;
-	
+
 	public TemplateFilesProvider() {
 		List<TemplateLoader> loaders = new ArrayList<TemplateLoader>();
-		String homeTemplatesDirPath = System.getProperty("user.home")+ "/.godiet/templates/";
-		File homeTemplatesDirnew = new File(homeTemplatesDirPath);
-		try {
-			loaders.add(new FileTemplateLoader(homeTemplatesDirnew,true));
-		} catch (IOException e) {
-			log.warn("Unable to load template from user home " + e.getMessage());
+		// Load from TEMPLATE_DIRECTORY_PATH
+		if (TEMPLATE_DIRECTORY_PATH != null) {
+			File homeTemplatesDirnew = new File(TEMPLATE_DIRECTORY_PATH);
+			try {
+				loaders.add(new FileTemplateLoader(homeTemplatesDirnew, true));
+			} catch (IOException e) {
+				log.warn("Unable to load template from directory. Perhaps  TEMPLATE_DIRECTORY_PATH env var not initialized correctly ? Currently is "+ TEMPLATE_DIRECTORY_PATH);
+				log.debug(e.getMessage());
+			}
+		}else{
+			log.warn("TEMPLATE_DIRECTORY_PATH not set. Only take the defaults template");
 		}
-		loaders.add(new ClassTemplateLoader(getClass(),"/configuration/templates"));
-		this.multiTemplateLoader = new MultiTemplateLoader(loaders.toArray(new TemplateLoader[0]));
-		
-	}
-	
+		// Load defaults templates from classpath
+		loaders.add(new ClassTemplateLoader(getClass(),
+				"/configuration/templates"));
+		this.multiTemplateLoader = new MultiTemplateLoader(
+				loaders.toArray(new TemplateLoader[0]));
 
+	}
 
 	public TemplateLoader getTemplateLoader() {
 		return multiTemplateLoader;
 	}
-	
+
 }
