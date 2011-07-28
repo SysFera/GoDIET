7b5c5b56fda3f8d379885a6aa3f932a64ccf82a9
pmartinez <philippe.martinez@sysfera.com>
2011-07-28 11:03:35 +0200

    Exposition of configuration templates files to the user. Server distribution now create templtes directory which include all templates. Could update (at runtime) template files

--- assemblies/run-server.sh
+++ assemblies/run-server.sh
@@ -1,1 +1,6 @@
+#!/bin/sh
+
+#Where user can create and overload the agents/seds configuration files
+TEMPLATE_DIRECTORY_PATH=`pwd`/templates
+echo "Template directory is : " $TEMPLATE_DIRECTORY_PATH
 java -classpath classes:lib/* com.sysfera.godiet.core.GodietServerMain
